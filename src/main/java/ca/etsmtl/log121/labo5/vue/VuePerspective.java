dcb1c7e445ace8a9232dfb4b8bc57cd31ff9be7a
Aicha Bnousinane <2314791@crosemont.qc.ca>
2026-03-26 21:58:24 -0400

    Feat: implementation des classes VuePerspective et VueVignette avec le patron observateur

--- src/main/java/ca/etsmtl/log121/labo5/vue/VuePerspective.java
+++ src/main/java/ca/etsmtl/log121/labo5/vue/VuePerspective.java
@@ -1,5 +1,42 @@
 package ca.etsmtl.log121.labo5.vue;
 
-public class VuePerspective {
+import ca.etsmtl.log121.labo5.modele.ImageModele;
+import ca.etsmtl.log121.labo5.modele.Observateur;
+import ca.etsmtl.log121.labo5.modele.Perspective;
+import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 
+public class VuePerspective extends BorderPane implements Observateur {
+
+    private final ImageModele imageModele;
+    private final Perspective perspective;
+    private final ImageView imageView;
+
+    public VuePerspective(ImageModele imageModele, Perspective perspective) {
+        this.imageModele = imageModele;
+        this.perspective = perspective;
+
+        imageModele.ajouterObservateur(this);
+        perspective.ajouterObservateur(this);
+
+        imageView = new ImageView();
+        imageView.setPreserveRatio(false);
+
+        ScrollPane scrollPane = new ScrollPane(imageView);
+        scrollPane.setPannable(true);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+
+        setCenter(scrollPane);
+        setPadding(new Insets(5));
+    }
+
+    @Override
+    public void miseAJour() {
+        Image img = imageModele.getImage();
+        imageView.setImage(img);
+    }
 }
--- src/main/java/ca/etsmtl/log121/labo5/vue/VueVignette.java
+++ src/main/java/ca/etsmtl/log121/labo5/vue/VueVignette.java
@@ -1,5 +1,42 @@
 package ca.etsmtl.log121.labo5.vue;
 
-public class VueVignette {
+import ca.etsmtl.log121.labo5.modele.ImageModele;
+import ca.etsmtl.log121.labo5.modele.Observateur;
+import javafx.geometry.Insets;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
 
+public class VueVignette extends BorderPane implements Observateur {
+
+    private static final double MAX_SIZE = 150.0;
+
+    private final ImageModele imageModele;
+    private final ImageView imageView;
+    private final ScrollPane scrollPane;
+
+    public VueVignette(ImageModele imageModele) {
+        this.imageModele = imageModele;
+        this.imageModele.ajouterObservateur(this);
+
+        imageView = new ImageView();
+        imageView.setPreserveRatio(true);
+        imageView.setFitWidth(MAX_SIZE);
+        imageView.setFitHeight(MAX_SIZE);
+
+        scrollPane = new ScrollPane(imageView);
+        scrollPane.setFitToWidth(true);
+        scrollPane.setFitToHeight(true);
+        scrollPane.setPrefSize(MAX_SIZE, MAX_SIZE);
+
+        setCenter(scrollPane);
+        setPadding(new Insets(5));
+    }
+
+    @Override
+    public void miseAJour() {
+        Image img = imageModele.getImage();
+        imageView.setImage(img);
+    }
 }
