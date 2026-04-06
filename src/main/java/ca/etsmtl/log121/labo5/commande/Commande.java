851627817971aa05cdcf6f24fbd578b08733d076
Farkunda Wahedi <farkunda.wahedi.1@ens.etsmtl.ca>
2026-04-06 16:37:24 -0400

    Feat: ajout des classes CommandeTranslation et CommandeZoom avec gestion des commandes

--- src/main/java/ca/etsmtl/log121/labo5/commande/Commande.java
+++ src/main/java/ca/etsmtl/log121/labo5/commande/Commande.java
@@ -1,5 +1,6 @@
 package ca.etsmtl.log121.labo5.commande;
 
 public interface Commande {
-
+    void executer(); //excecuter le changement (zoom/translation)
+    void annuler(); //undo le changement
 }
--- src/main/java/ca/etsmtl/log121/labo5/commande/GestionnaireCommande.java
+++ src/main/java/ca/etsmtl/log121/labo5/commande/GestionnaireCommande.java
@@ -1,5 +1,33 @@
 package ca.etsmtl.log121.labo5.commande;
 
+import java.util.Stack;
+
+//classe Singleton
 public class GestionnaireCommande {
 
+    private static GestionnaireCommande instance;
+    private Stack<Commande> historique;
+
+    private GestionnaireCommande() {
+        historique = new Stack<>();
+    }
+
+    public static GestionnaireCommande getInstance() {
+        if (instance == null) {
+            instance = new GestionnaireCommande();
+        }
+        return instance;
+    }
+
+    public void executerCommande(Commande commande) {
+        commande.executer();
+        historique.push(commande);
+    }
+
+    public void annulerDerniereCommande() {
+        if (!historique.isEmpty()) {
+            Commande derniereCommande = historique.pop();
+            derniereCommande.annuler();
+        }
+    }
 }
