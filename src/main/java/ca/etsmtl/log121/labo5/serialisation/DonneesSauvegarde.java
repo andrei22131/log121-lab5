c9debfbdf2db027adce8a8a49bbccf9b2e98a32d
Aicha Bnousinane <2314791@crosemont.qc.ca>
2026-04-14 13:07:12 -0400

    Ajout de la documentation pour le package sérialisation

--- src/main/java/ca/etsmtl/log121/labo5/serialisation/DonneesSauvegarde.java
+++ src/main/java/ca/etsmtl/log121/labo5/serialisation/DonneesSauvegarde.java
@@ -4,7 +4,6 @@ import ca.etsmtl.log121.labo5.modele.Perspective;
 
 import java.io.Serializable;
 
-//le chemin de l'image et les deux perspectives
 public class DonneesSauvegarde implements Serializable {
 
     private static final long serialVersionUID = 1L;
@@ -13,20 +12,38 @@ public class DonneesSauvegarde implements Serializable {
     private final Perspective perspective1;
     private final Perspective perspective2;
 
+    /**
+     * Crée un objet de sauvegarde avec une copie des perspectives.
+     * @param cheminImage chemin de l'image
+     * @param perspective1 première perspective
+     * @param perspective2 deuxième perspective
+     */
     public DonneesSauvegarde(String cheminImage, Perspective perspective1, Perspective perspective2) {
         this.cheminImage = cheminImage;
         this.perspective1 = perspective1.copier();
         this.perspective2 = perspective2.copier();
     }
 
+    /**
+     * Retourne le chemin de l'image.
+     * @return chemin de l'image
+     */
     public String getCheminImage() {
         return cheminImage;
     }
 
+    /**
+     * Retourne la première perspective sauvegardée.
+     * @return perspective 1
+     */
     public Perspective getPerspective1() {
         return perspective1;
     }
 
+    /**
+     * Retourne la deuxième perspective sauvegardée.
+     * @return perspective 2
+     */
     public Perspective getPerspective2() {
         return perspective2;
     }
--- src/main/java/ca/etsmtl/log121/labo5/serialisation/GestionnaireSauvegarde.java
+++ src/main/java/ca/etsmtl/log121/labo5/serialisation/GestionnaireSauvegarde.java
@@ -5,12 +5,25 @@ import java.io.*;
 //gère la sérialisation/désérialisation de l'état de l'application sur disque
 public class GestionnaireSauvegarde {
 
+    /**
+     * Sauvegarde les données dans un fichier.
+     * @param donnees données à sauvegarder
+     * @param fichier fichier de destination
+     * @throws IOException en cas d'erreur d'écriture
+     */
     public static void sauvegarder(DonneesSauvegarde donnees, File fichier) throws IOException {
         try (ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(fichier))) {
             oos.writeObject(donnees);
         }
     }
 
+    /**
+     * Charge les données depuis un fichier.
+     * @param fichier fichier source
+     * @return données chargées
+     * @throws IOException en cas d'erreur de lecture
+     * @throws ClassNotFoundException si la classe n'est pas trouvée
+     */
     public static DonneesSauvegarde charger(File fichier) throws IOException, ClassNotFoundException {
         try (ObjectInputStream ois = new ObjectInputStream(new FileInputStream(fichier))) {
             return (DonneesSauvegarde) ois.readObject();
